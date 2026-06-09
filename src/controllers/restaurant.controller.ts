10b13aee12cab047297feae2d34ea4bfb585e42b
Muzaffar <itsmuuza@icloud.com>
2026-06-09 16:41:51 +0900

    feat: develop restaurant login process

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { MemberInput } from "../libs/types/member";
+import { LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const restaurantController: T = {};
@@ -30,11 +30,16 @@ restaurantController.getSignup = (req: Request, res: Response) => {
   }
 };
 
-restaurantController.processLogin = (req: Request, res: Response) => {
+restaurantController.processLogin = async (req: Request, res: Response) => {
   try {
-    res.send("post: admin login process");
+    console.log(req.body);
+    const input: LoginInput = req.body;
+    const memberService = new MemberService();
+    const result = await memberService.processLogin(input);
+    res.send(result);
   } catch (err) {
     console.log("admin login process:", err);
+    res.send(err);
   }
 };
 
--- src/libs/Errors.ts
+++ src/libs/Errors.ts
@@ -14,6 +14,9 @@ export enum Message {
   NO_DATA_FOUND = "No data is found!",
   CREATE_FAILED = "Create is failed!",
   UPDATE_FAILED = "Update is failed!",
+  NO_MEMBER_NICK = "No member with that member nick!",
+  USED_NICK_PHONE = "You are inserting already used nick or phone!",
+  WRONG_PASSWORD = "Wrong password, please try again!",
 }
 
 class Errors extends Error {
